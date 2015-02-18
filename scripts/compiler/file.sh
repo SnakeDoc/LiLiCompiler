07bb20b674351b230b5ff70da98366ea75820866
Jason Sipula <alupis1@gmail.com>
2015-02-18 10:00:15 -0800

    add file utility to toolchain

---
+++ scripts/compiler/file.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# file
+
+set -e
+set -u
+
+. settings/config
+. scripts/utils/utils.sh
+
+pkg_dir="$(locate_package 'file')"
+
+. "${pkg_dir}/package.mk"
+
+cd "${CLFS_SOURCES}/"
+if [ ! -e "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}.tar.gz" ]; then
+    wget --read-timeout=20 "${PKG_URL}"
+fi
+
+cd "${CLFS_SOURCES}/"
+# make sure things are clean
+if [ -d "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}" ]; then
+    rm -rf "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}"
+fi
+
+# business time
+cd "${CLFS_SOURCES}/"
+tar -zxvf "${PKG_NAME}-${PKG_VERSION}.tar.gz"
+
+cd "${CLFS_SOURCES}/${PKG_NAME}-${PKG_VERSION}/"
+
+./configure "${PKG_CONFIGURE_OPTS[@]}"
+  
+make
+
+make install
+
+# cleanup
+cd "${CLFS_SOURCES}/"
+rm -rf "${PKG_NAME}-${PKG_VERSION}"
+
+exit 0
+
