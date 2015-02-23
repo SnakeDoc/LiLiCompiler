62d26c434e0119f3cbba8761ab2bc54105a53e12
Jason Sipula <alupis1@gmail.com>
2015-02-22 17:03:11 -0800

    tweak gmp package

--- packages/devel/gmp/package.mk
+++ packages/devel/gmp/package.mk
@@ -22,6 +22,6 @@ PKG_URL="http://ftp.gnu.org/gnu/gmp/${PKG_NAME}-${PKG_VERSION}${PKG_SUB_VERSION}
 PKG_DEPENDS=""
 PKG_SECTION="devel"
 
-PKG_CONFIGURE_OPTS=(--build="${CLFS_HOST}"
-                    --host="${CLFS_TARGET}"
-                    --prefix="${CLFS_TOOLS}/${CLFS_TARGET}")
+PKG_CONFIGURE_OPTS=(--prefix="${CLFS_TOOLS}"
+                    --enable-cxx
+                    --disable-static)
